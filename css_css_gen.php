4852e0ea1c93596b1e923fdffa73e17c3db3b64c
Franck S. <franck@emagma.fr>
2026-06-17 14:49:50 +0200

    update(tree): add colored svg icons

--- css_css_gen.php
+++ css_css_gen.php
@@ -19,8 +19,8 @@ $folders = [
     [ "name" => "javascript",   "color" => "#f7df1e", "textColor" => "#000000"],
     [ "name" => "linux",        "color" => "#fcc624", "textColor" => "#000000"],
     [ "name" => "llm",          "color" => "#06b6d4", "textColor" => "#000000"],
-    [ "name" => "lua",          "color" => "#000080", "textColor" => "#000000"],
-    [ "name" => "markdown",     "color" => "#ffffff", "textColor" => "#000000"],
+    [ "name" => "lua",          "color" => "#000080", "textColor" => "#ffffff"],
+    [ "name" => "markdown",     "color" => "#ffffff",  "textColor" => "#000000"],
     [ "name" => "mermaid",      "color" => "#ff3785", "textColor" => "#000000"],
     [ "name" => "obsidian",     "color" => "#8B5CF6", "textColor" => "#ffffff"],
     [ "name" => "php",          "color" => "#777BB3", "textColor" => "#ffffff"],
@@ -35,24 +35,59 @@ $folders = [
     [ "name" => "windows",      "color" => "#00a4ef", "textColor" => "#000000"],
 ];
 
-function generateBlock(string $name, string $color, string $textColor): string {
-    $title = ucwords($name);
-    return "/* $title */\n"
-        . ".nav-folder-title[data-path*=\"$name\" i] ~ .nav-folder-children { border-left: 1px solid $color; }\n"
-        . ".nav-folder-title[data-path*=\"$name\" i] .tree-item-inner,\n"
-        . ".nav-file-title[data-path*=\"$name\" i] .tree-item-inner {\n"
-        . "\tbackground-color: $color;\n"
-        . "\tcolor: $textColor;\n"
-        . "}\n";
+// Folder svg
+function svgFolder(string $color): string {
+    $svg = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="' . $color . '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>';
+    return 'url("data:image/svg+xml,' . rawurlencode($svg) . '")';
 }
 
-$common = ".nav-folder-title .tree-item-inner,\n"
+// File svg
+function svgFile(string $color): string {
+    $svg = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="' . $color . '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14.364 13.634a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506l4.013-4.009a1 1 0 0 0-3.004-3.004z"/><path d="M14.487 7.858A1 1 0 0 1 14 7V2"/><path d="M20 19.645V20a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l2.516 2.516"/><path d="M8 18h1"/></svg>';
+    return 'url("data:image/svg+xml,' . rawurlencode($svg) . '")';
+}
+
+function generateBlock(string $name, string $color, string $textColor): string {
+    $title = ucwords($name);
+    $folderIcon = svgFolder($color);
+    $fileIcon   = svgFile($color);
+    return "/* --- $title --- */\n"
+        . ".nav-folder-title[data-path*=\"$name\" i] .tree-item-inner,\n"
+        . ".nav-file-title[data-path*=\"$name\" i] .tree-item-inner { background-color: $color; color: $textColor;}\n"
+        . ".nav-folder-title[data-path*=\"$name\" i] ~ .nav-folder-children { border-left: 1px solid $color; }\n"
+        . ".nav-folder-title[data-path*=\"$name\" i]::before { content: $folderIcon; }\n"
+        . ".nav-file-title[data-path*=\"$name\" i]::before   { content: $fileIcon; }\n";
+}
+
+$defaultFolder = svgFolder('#888888');
+$defaultFile   = svgFile('#888888');
+
+$common = "/* Common styles */\n"
+        . ".nav-folder-title .tree-item-inner,\n"
         . ".nav-file-title .tree-item-inner {\n"
         . "\tborder-radius: 4px;\n"
         . "\tpadding: 2px 6px;\n"
         . "\tdisplay: block;\n"
         . "\twidth: 100%;\n"
         . "\tbox-sizing: border-box;\n"
+        . "}\n\n"
+        . "/* Default icons */\n"
+        . ".nav-folder-title .nav-folder-collapse-indicator { display: none; }\n"
+        . ".nav-folder-title::before {\n"
+        . "\tcontent: $defaultFolder;\n"
+        . "\tdisplay: inline-block;\n"
+        . "\twidth: 14px;\n"
+        . "\theight: 14px;\n"
+        . "\tmargin-right: 6px;\n"
+        . "\tvertical-align: middle;\n"
+        . "}\n"
+        . ".nav-file-title::before {\n"
+        . "\tcontent: $defaultFile;\n"
+        . "\tdisplay: inline-block;\n"
+        . "\twidth: 14px;\n"
+        . "\theight: 14px;\n"
+        . "\tmargin-right: 6px;\n"
+        . "\tvertical-align: middle;\n"
         . "}\n\n";
 
 $output = $common . implode("\n", array_map(
